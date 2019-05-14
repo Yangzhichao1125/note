e65e2660c695bbf1559a46b038fa2dabe45d3654
杨志超 <307823443@qq.com>
2019-05-14 22:38:07 +0800

    piano

---
+++ life/piano/js/jq223.js
@@ -0,0 +1,36 @@
+﻿
+
+	window.onload = function(){
+		var i;
+		var pianoArea = document.getElementById('pianoArea');
+	
+	
+	//指定发出的声音
+	function playSound(noteNumber){
+		var soundId = 'sound' + noteNumber;
+		var keyId = 'key' + noteNumber;
+		var key = document.getElementById(keyId);
+		var audio = null;
+		var audio = new Audio();
+			audio.src = 'music/'+noteNumber+'.mp3'  ;
+			audio.play();
+		
+	}
+
+	
+	
+	//按下键盘时
+	document.onkeydown = function(e) {
+		var pressEvent = e || window.event;
+		var keyCode = '';
+		if(pressEvent.keyCode){
+			keyCode = pressEvent.keyCode;
+		}else if(pressEvent.charCode){
+			keyCode = pressEvent.charCode;
+		}else if(pressEvent.which){
+			keyCode = pressEvent.which;
+		}
+		console.log(keyCode);           
+		playSound(keyCode);
+	}
+}
